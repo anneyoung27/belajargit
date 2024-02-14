66b4d8cf9ccb48884af240b572540c8b6cb322e6
Injas Mahendra Berutu <injasmahendra123@gmail.com>
2024-02-14 17:18:38 +0700

    updated by C

--- Test.java
+++ Test.java
@@ -4,6 +4,15 @@ public class Test{
         int b = 7; 
         double c = 12.0; // you missed to initialize variable c!
 
+        // using array #updated by C;
+        int [] arrNum = {a, b, (int) c};
+        int sum = 0;
+
+        for (int totalNum : arrNum){
+            sum += totalNum;
+        }
+        System.out.println("Total with Array : " + sum);
+        
         int total = (int) ((a + b) / c);
         System.out.println("Total : " + total);
     }
