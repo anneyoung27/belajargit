974c671ec69b3bf3418fc897dfa77ff8e58d8e65
Injas Mahendra Berutu <injasmahendra123@gmail.com>
2024-02-14 16:49:27 +0700

    first commit 2024/02/14

---
+++ Test.java
@@ -0,0 +1,9 @@
+class Test{
+    public static void main(String [] args){
+        int a = 5;
+        int b = 7; 
+
+        int total = a + b;
+        System.out.println("Total :" + total);
+    }
+}
